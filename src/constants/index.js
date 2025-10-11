1ea7df0a4922871749fda8ba6baacb6685e07f62
Iryna Buhela <developer.buhela@gmail.com>
2025-10-11 21:59:25 +0300

    Fix: correct path to reset-password-email.html template for Render deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,7 +16,7 @@ export const SMTP = {
   SMTP_FROM: 'SMTP_FROM',
 };
 
-export const TEMP_DIR = path.join(process.cwd(), 'src', 'temp');
+export const TEMP_DIR = path.join(process.cwd(), 'src', 'templates');
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
